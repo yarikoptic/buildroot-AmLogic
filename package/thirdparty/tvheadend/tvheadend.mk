91286f3b28208c66e6e2a1fe6fecb966e943b2be
J1nx <admin@j1nx.nl>
2013-03-20 20:21:27 +0000

    [all] TVheadend package updates

--- package/thirdparty/tvheadend/tvheadend.mk
+++ package/thirdparty/tvheadend/tvheadend.mk
@@ -4,14 +4,12 @@
 #
 ##############################################################
 
-TVHEADEND_VERSION           = hardware/tbsmoi
-TVHEADEND_SITE              = git://github.com/tvheadend/tvheadend.git
-TVHEADEND_LICENSE           = GPLv3+
-TVHEADEND_LICENSE_FILES     = LICENSE
-TVHEADEND_DEPENDENCIES      = host-pkgconf host-python openssl
-
-# Configure options and dependencies
-TVHEADEND_CONFIGURE_OPTS    = --enable-bundle --disable-libav
+TVHEADEND_VERSION = 3.0
+TVHEADEND_SITE_METHOD = git
+TVHEADEND_SITE = git://github.com/tvheadend/tvheadend.git
+TVHEADEND_INSTALL_STAGING = YES
+TVHEADEND_INSTALL_TARGET = YES
+TVHEADEND_DEPENDENCIES = v4lutils openssl
 
 ifeq ($(BR2_PACKAGE_AVAHI),y)
 TVHEADEND_DEPENDENCIES     += avahi
@@ -27,31 +25,21 @@ else
 TVHEADEND_CONFIGURE_OPTS   += --disable-imagecache
 endif
 
-define TVHEADEND_CONFIGURE_CMDS
-  (cd $(TVHEADEND_DIR); \
-  ./support/changelog ./debian/changelog buildroot $(TVHEADEND_VERSION); \
-   $(TARGET_CONFIGURE_OPTS) \
-   $(TARGET_CONFIGURE_ARGS) \
-   ./configure \
-  --prefix=/usr \
-  $(TVHEADEND_CONFIGURE_OPTS) \
-  )
-endef
-
 #----------------------------------------------------------------------------
 # To run tvheadend, we need:
-#  - a startup script, and its config file
+#  - a startup script
 #  - a default DB with a tvheadend admin
 define TVHEADEND_INSTALL_DB
- $(INSTALL) -D package/tvheadend/accesscontrol.1     \
-               $(TARGET_DIR)/root/.hts/tvheadend/accesscontrol/1
+ $(INSTALL) -D package/thirdparty/tvheadend/accesscontrol.1     \
+               $(TARGET_DIR)/root/.xbmc/tvheadend/accesscontrol/1
 endef
-TVHEADEND_POST_INSTALL_TARGET_HOOKS  = TVHEADEND_INSTALL_DB
 
 define TVHEADEND_INSTALL_INIT_SYSV
- $(INSTALL) -D $(TVHEADEND_DIR)/debian/tvheadend.default $(TARGET_DIR)/etc/default/tvheadend
- $(INSTALL) -D package/tvheadend/S99tvheadend $(TARGET_DIR)/etc/init.d/S99tvheadend
+ $(INSTALL) -D package/thirdpart/tvheadend/S99tvheadend $(TARGET_DIR)/etc/init.d/S99tvheadend
 endef
+
+TVHEADEND_POST_INSTALL_TARGET_HOOKS  += TVHEADEND_INSTALL_DB
+TVHEADEND_POST_INSTALL_TARGET_HOOKS  += TVHEADEND_INSTALL_INIT_SYSV
 
 #----------------------------------------------------------------------------
 # tvheadend is not an autotools-based package, but it is possible to
