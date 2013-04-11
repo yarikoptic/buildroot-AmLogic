276d885bbce77197b5f1608d48d2b6a2b695bda1
J1nx <admin@j1nx.nl>
2013-04-11 12:57:25 +0000

    [all] Use ilia his remote.conf and keyboard.xml to asign all buttons to actions

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -59,6 +59,7 @@ define XBMC_INSTALL_ETC
   cp -f package/thirdparty/xbmc/advancedsettings.xml $(TARGET_DIR)/usr/share/xbmc/system/
   cp -f package/thirdparty/xbmc/nobs.xml $(TARGET_DIR)/usr/share/xbmc/system/keymaps/
   cp -f package/thirdparty/xbmc/mouse.xml $(TARGET_DIR)/usr/share/xbmc/system/keymaps/
+  cp -f package/thirdparty/xbmc/keyboard.xml $(TARGET_DIR)/usr/share/xbmc/system/keymaps/
 endef
 
 define XBMC_INSTALL_REMOTE_CONF
